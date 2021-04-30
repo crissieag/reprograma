1f36262d26ebfe64f5d6bfa2eca5f7bb7d55ae75
Crissie A Giese <crissieagiese@gmail.com>
2021-04-30 13:54:36 -0300

    primeiro commit

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validaFormulario();
+});
+
+function validaFormulario() {
+  let formulario = document.getElementById("form");
+  let nome = document.getElementById("username").value;
+  let email = document.getElementById("email").value;
+  let erroNome = document.getElementById("erroNome");
+  let erroEmail = document.getElementById("erroEmail");
+
+  let padrao = /\S+@\S+\.\S+/;
+  let nomeValido = nome || nome.length >= 2;
+  let emailValido = email.match(padrao);
+
+  if (nomeValido) {
+    erroNome.classList.remove("validaErro");
+  } else {
+    erroNome.classList.add("validaErro");
+  }
+
+  if (emailValido) {
+    erroEmail.classList.remove("validaErro");
+  } else {
+    erroEmail.classList.add("validaErro");
+  }
+
+  // modal
+  if (nomeValido && emailValido) {
+    let modal = document.getElementById("myModal");
+    let span = document.getElementById("fechaModal");
+
+    let fecharModal = function () {
+      modal.style.display = "none";
+    };
+
+    span.onclick = fecharModal;
+
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        fecharModal();
+      }
+    };
+
+    modal.style.display = "block";
+    document.getElementById("modalMensagem").innerHTML =
+      "Olá " +
+      nome +
+      " recebemos seus dados! As novidades serão enviadas para seu email: " +
+      email +
+      " .";
+    formulario.reset();
+  }
+}
